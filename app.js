c5e45702c85b7002d81630e6ca8f43bd801a181d
Adlan Yandarbiev <vainakh@MacBook-Pro.local>
2020-04-24 13:13:59 -0700

    Add heroku configuration

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.post("/failure", function(req, res){
 })
 
 
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("The server is running on port 3000.");
 });
 
